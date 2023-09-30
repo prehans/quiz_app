94200ee838e76824132b10eaa6df1d444249f9f8
prehans <prehansgupta@gmail.com>
2023-09-30 23:12:29 +0530

    updated path

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const userRoutes = require("./routes/userRoutes");
 const quizRoutes = require("./routes/quizRoutes");
 const performanceRoutes = require("./routes/performanceRoutes");
 const cors = require("cors");
+const path=require("path");
+
+
+
 
 dotenv.config();
 
 connectDb();
 
 const app = express();
-
+app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.get("*",function (req,res){
+  res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
+})
 app.use(express.json());
 
 app.use(cors());
