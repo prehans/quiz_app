997e45abd4e3e53e7dad364a5d2110d9f9c9ac06
prehans <prehansgupta@gmail.com>
2023-09-30 22:58:58 +0530

    done

---
+++ frontend/src/pages/Mainpage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import Navbar from "../components/others/navbar";
+
+const Mainpage = () => {
+  return (
+    <>
+      <Navbar />
+      <Box
+        minH="100vh"
+        display="flex"
+        justifyContent="center"
+        textAlign="center"
+        flexDirection="column"
+        mx={{ base: "0", md: "auto" }}
+        p={4}
+      >
+        <Heading
+          as="h1"
+          fontSize={{ base: "4xl", md: "5xl" }}
+          color="white"
+          mb={4}
+        >
+          Welcome to the Language Learning Quiz App!
+        </Heading>
+        <Text fontSize={{ base: "xl", md: "2xl" }} color="white" mb={8}>
+          Improve your language skills through fun quizzes.
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default Mainpage;
